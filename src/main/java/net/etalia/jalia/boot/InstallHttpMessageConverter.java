0b40853efdddfd41f0547fb54313a6febdf1963a
Simone <simoneg@apache.org>
2020-01-07 20:11:57 +0000

    Use spring DelegatingWebMvcConfiguration to respect auto configuration

--- src/main/java/net/etalia/jalia/boot/InstallHttpMessageConverter.java
+++ src/main/java/net/etalia/jalia/boot/InstallHttpMessageConverter.java
@@ -12,11 +12,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
 
 @Configuration
 @AutoConfigureBefore(WebMvcAutoConfiguration.class)
-public class InstallHttpMessageConverter extends WebMvcConfigurationSupport {
+public class InstallHttpMessageConverter extends DelegatingWebMvcConfiguration {
 
     private final static Logger LOG = Logger.getLogger(InstallHttpMessageConverter.class.getName());
 
